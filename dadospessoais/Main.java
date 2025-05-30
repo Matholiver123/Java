332a708b2bd6b15d65784d4a2b7bd9744af33491
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-05-30 10:50:18 -0300

    Add files via upload

---
+++ dadospessoais/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+      String nome = "Matheus";
+      int idade = 17;
+      double altura = 1.81;
+      boolean gostaDeFutebol  = true;
+
+      System.out.println("Nome:" + nome);
+      System.out.println("Idade:" + idade);
+      System.out.println("Altura:" + altura);
+      System.out.println("Gosta de Futebol:" + gostaDeFutebol);
+
+      
+      
+
+
+    }
+  }
