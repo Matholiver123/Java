794bdfe00b2f05a5cf4ce4e978e7aefdcdfe2c29
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-05-30 10:58:04 -0300

    Add files via upload

---
+++ calculadora/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+      int num1 = 20;
+      int num2 = 5;
+      int soma;
+      int subtracao;
+      int multiplicacao;
+      int divisao;
+
+      soma = num1 + num2;
+      subtracao = num1 - num2;
+      multiplicacao = num1 * num2;
+      divisao = num1 /num2;
+
+      System.out.println("Soma:" + soma);
+      System.out.println("Subtração:" + subtracao);
+      System.out.println("Multiplicação:" + multiplicacao);
+      System.out.println("Divisão:" + divisao);
+    }
+  }
