105fb8a311f74df47fefa2b0378a4947c627229c
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-05-30 11:08:27 -0300

    Add files via upload

---
+++ media/media.java
@@ -0,0 +1,12 @@
+public class media {
+    public static void main(String[] args) {
+      float nota1 = 4.0f;
+      float nota2 = 6.0f;
+      float nota3 = 3.0f;
+       float media;
+       media = (nota1 + nota2 + nota3)/3;
+       System.out.println("A média do aluno será:" + media);
+
+
+    }
+  }
