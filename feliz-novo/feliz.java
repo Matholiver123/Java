664861cd2eeb8d1d3a6c44a3f753dd041735022c
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-05-31 13:18:48 -0300

    Add files via upload

---
+++ feliz-novo/feliz.java
@@ -0,0 +1,12 @@
+public class feliz {
+    public static void main(String[] args) {
+      int i = 10;
+     
+    while(i >= 1) {
+        System.out.println(i);
+        i--;
+    }
+    System.out.println("Feliz ano novo");
+  
+    }
+  }
