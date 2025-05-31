16c646002c4fb17e4ae0ec178caf7d5d81b9fb51
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-05-31 13:00:51 -0300

    Add files via upload

---
+++ Classificação-de-nota/notas.java
@@ -0,0 +1,35 @@
+public class notas {
+    public static void main(String[] args) {
+      int nota = 10;
+      switch (nota) {
+        case 10:
+        System.out.println("Excelente");
+          break;
+          case 9:
+          case 8:
+    System.out.println("Muito bom");
+    break;
+   
+  case 7:
+  case 6:
+    System.out.println("Bom");
+    break;
+  case 5:
+  case 4:
+  case 3:
+  case 2:
+  case 1:
+    System.out.println("Precisa melhorar");
+    break;
+ 
+      
+        default:
+          break;
+      }
+
+      
+      
+
+
+    }
+  }
