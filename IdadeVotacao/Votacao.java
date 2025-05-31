7001f4376c13de3c90b83d37240894ee6dd726c5
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-05-31 13:01:13 -0300

    Add files via upload

---
+++ IdadeVotacao/Votacao.java
@@ -0,0 +1,13 @@
+public class Votacao {
+    public static void main(String[] args) {
+      int idade = 15;
+     
+      if (idade >= 16) {
+        System.out.println("Pode votar nas eleições");
+      } else{
+        System.out.println("Não pode votar nas eleições");
+      
+  
+    }
+}
+}
