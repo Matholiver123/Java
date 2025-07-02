7cee278171d06047d07589190e6012db0652fa7b
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-07-02 16:52:47 -0300

    Add files via upload

---
+++ Mensagem/main.java
@@ -0,0 +1,12 @@
+public class main{
+    static void exibirMensagem(){
+        System.out.println("Bem-vindo(a) ao curso de Java!");
+    }
+    public static void main(String[] args) {
+    exibirMensagem();
+    exibirMensagem();
+    exibirMensagem();
+
+}
+}
+    
