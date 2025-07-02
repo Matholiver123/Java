85b99796e086292cf7a9a8a5ef4b385d4dfcaeda
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-07-02 16:51:05 -0300

    Add files via upload

---
+++ Saudacao/saudacao.java
@@ -0,0 +1,16 @@
+public class saudacao {
+    static void dizerOla(String nome){
+        System.out.println("Olá, " + nome + "! Seja bem-vindo(a)");
+
+        
+    }
+        
+    public static void main(String[] args) {
+      dizerOla("Matheus");
+      dizerOla("Nicolas");
+      dizerOla("Isabelly");
+
+    
+}
+
+}
